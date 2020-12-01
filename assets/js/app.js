9f17f99b6997f4d3590112d03ef216b3e43743e6
Pankaj <tahiliani.pankaj19@gmail.com>
2020-12-01 13:09:26 -0800

    setting js margins and dimensions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
 // @TODO: YOUR CODE HERE!
+
+// Determing width and Height
+var svgWidth = 960;
+var svgHeight = 500;
+
+
+// Setting Margins
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
