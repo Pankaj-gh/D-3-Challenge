cf56f18f449833685929cea6886d244da312e4cb
Pankaj <tahiliani.pankaj19@gmail.com>
2020-12-01 13:13:19 -0800

    creating svg wrapper

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,13 @@ var margin = {
 // Chart width and Height
 var chartWidth = svgWidth - margin.left - margin.right;
 var chartHeight = svgHeight - margin.top - margin.bottom;
+
+
+//  SVG wrapper, append an SVG group to hold chart & shift the it left & top
+var svg = d3.select(".scatter-plot")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
